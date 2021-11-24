26cf31889731d397b33aa8220748ff4ef3869031
stefrie <sriehle@gmail.com>
2021-11-23 21:52:46 -0800

    add code + test

---
+++ index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+function merge(arr1, arr2) {
+	let sorted = [];
+
+	while (arr1.length && arr2.length) {
+		if (arr1[0] < arr2[0]) sorted.push(arr1.shift());
+		else sorted.push(arr2.shift());
+	}
+	return sorted.concat(arr1.slice().concat(arr2.slice()));
+}
+
+function mergeSort(arr) {
+	if (arr.length <= 1) return arr;
+	let mid = Math.floor(arr.length / 2),
+		left = mergeSort(arr.slice(0, mid)),
+		right = mergeSort(arr.slice(mid));
+
+	return merge(left, right);
+}
+
+console.log(mergeSort);
+
---
+++ tests.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const { mergeSort } = require('./index.js');
+
+describe('Merge Sort', () => {
+	it('should sort an array numerically', () => {
+		let arr = [8, 4, 23, 42, 16, 15];
+		let sortedArr = [4, 8, 15, 16, 23, 42];
+		expect(mergeSort(sortedArr).toEqual(arr));
+	});
+});
